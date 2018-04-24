1098c0c47723976c3478ee87847d5d92b8231956
shu <sus5@seattleu.edu>
2018-04-24 00:45:21 -0700

    hw files

--- hw2data.js
+++ hw2data.js
@@ -1,20 +1,109 @@
 {
   "data": [
     {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "email": "3814"
+      "name": "Olivia Adams",
+      "job title": "Library Associate",
+      "salary": "$55,750",
+      "location": "Seattle",
+      "company": "Seattle Public Library"
     },
     {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "email": "9497"
+      "name": "Sheema Ali",
+      "job title": "Product Manager",
+      "salary": "$185,000",
+      "location": "San Francisco",
+      "company": "Google"
+    },
+    {
+      "name": "Diana Bailey",
+      "job title": "Sales Engineer",
+      "salary": "$86,000",
+      "location": "Boeing",
+      "company": "1562"
+    },
+    {
+      "name": "Erika Arroyo",
+      "job title": "Senior Accountant",
+      "salary": "$72,700",
+      "location": "Seattle",
+      "company": "KPMG"
+    },
+    {
+      "name": "Jeffery Barry",
+      "job title": "English Teacher",
+      "salary": "$46,450",
+      "location": "San Francisco",
+      "company": "Gateway High School"
+    },
+    {
+      "name": "Karina Olivera",
+      "job title": "Business Operations Analyst",
+      "salary": "$120,000",
+      "location": "Seattle",
+      "company": "Amazon"
+    },
+    {
+      "name": "Christopher Olszanski",
+      "job title": "Financial Analyst",
+      "salary": "$83,060",
+      "location": "Seattle",
+      "company": "PwC"
+    },
+    {
+      "name": "Terry Normand",
+      "job title": "Human Resource Analyst",
+      "salary": "$83,500",
+      "location": "Seattle",
+      "company": "Swedish Medical Center"
+    },
+    {
+      "name": "Jason Kim",
+      "job title": "Software Developer",
+      "salary": "$205,500",
+      "location": "San Francisco",
+      "company": "Facebook"
+    },
+    {
+      "name": "Tiffany Myers",
+      "job title": "Marketing Designer",
+      "salary": "$167,500",
+      "location": "San Francisco",
+      "company": "Facebook"
+    },
+    {
+      "name": "Sam Murray",
+      "job title": "Sales Assistant",
+      "salary": "$67,500",
+      "location": "San Francisco",
+      "company": "Sephora"
+    },
+    {
+      "name": "Louis Morales",
+      "job title": "Police Officer",
+      "salary": "$62,900",
+      "location": "Seattle",
+      "company": "City of Seattle"
+    },
+    {
+      "name": "Nicholas Moore",
+      "job title": "Financial Analyst",
+      "salary": "$103,600",
+      "location": "San Francisco",
+      "company": "Google"
+    },
+    {
+      "name": "Dilan Huang",
+      "job title": "Software Engineer",
+      "salary": "$140,560",
+      "location": "San Francisco",
+      "company": "Indeed"
+    },
+    {
+      "name": "Ben Orenstein",
+      "job title": "Data Analyst",
+      "salary": "$124,000",
+      "location": "Seattle",
+      "company": "Zillow"
     }
   ]
 }
