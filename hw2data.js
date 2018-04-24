5e27e7f0e444454b4fbad78c558f5674a9c31f71
shu <sus5@seattleu.edu>
2018-04-23 20:43:08 -0700

    hw files

---
+++ hw2data.js
@@ -0,0 +1,20 @@
+{
+  "data": [
+    {
+      "name": "Jena Gaines",
+      "designation": "Office Manager",
+      "salary": "$90,560",
+      "joining_date": "2008/12/19",
+      "office": "London",
+      "email": "3814"
+    },
+    {
+      "name": "Quinn Flynn",
+      "designation": "Support Lead",
+      "salary": "$342,000",
+      "joining_date": "2013/03/03",
+      "office": "Edinburgh",
+      "email": "9497"
+    }
+  ]
+}
